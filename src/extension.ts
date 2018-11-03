6ffb66c658c9f0e7678bd012d842e6a1e2f86d74
Your Name <stephan.seitz@fau.de>
2018-11-03 21:11:32 +0100

    Fix issue #2: consider case "first letter capitalized"

--- src/extension.ts
+++ src/extension.ts
@@ -12,11 +12,18 @@ const swapText = (textBlob: string): string => {
     const re = new RegExp(Object.keys(mapObj).join('|'), 'gi');
     const textBlobWithReplacements = textBlob.replace(re, matched => {
       const isUpperCase = matched === matched.toUpperCase();
+      const isFirstUpperCase = matched === (matched.charAt(0).toUpperCase() + matched.slice(1));
       const currentItem = mapObj[matched.toLowerCase()];
       if (currentItem === undefined) return matched; // unneccessary, but just to check.
-      const match = isUpperCase
-        ? currentItem.toString().toUpperCase()
-        : currentItem;
+      const match = (() => {
+        if (isUpperCase) {
+          return currentItem.toString().toUpperCase();
+        } else if (isFirstUpperCase) {
+          return (currentItem.toString().charAt(0).toUpperCase() + currentItem.toString().slice(1));
+        } else {
+          return currentItem;
+        }
+      })();
       return match;
     });
     return textBlobWithReplacements;
