f37f4ed190e8b2f26129e16b96f904876ca4147e
Seth Silesky <seth.silesky@gmail.com>
2018-11-04 14:38:55 -0600

    move swapText to utils

--- src/extension.ts
+++ src/extension.ts
@@ -1,47 +1,11 @@
 import * as vscode from 'vscode';
+import { swapText } from './utils';
 
 const hasActiveSelections = (editor: vscode.TextEditor): boolean => {
   if (editor.selections.length > 1) {
     return true;
   }
   return !editor.selection.isEmpty;
-};
-
-const swapText = (textBlob: string): string => {
-  const findAndReplace = (textBlob, mapObj) => {
-    const re = new RegExp(Object.keys(mapObj).join('|'), 'gi');
-    const textBlobWithReplacements = textBlob.replace(re, matched => {
-      const isUpperCase = matched === matched.toUpperCase();
-      const isFirstUpperCase = matched === (matched.charAt(0).toUpperCase() + matched.slice(1));
-      const currentItem = mapObj[matched.toLowerCase()];
-      if (currentItem === undefined) return matched; // unneccessary, but just to check.
-      const match = (() => {
-        if (isUpperCase) {
-          return currentItem.toString().toUpperCase();
-        } else if (isFirstUpperCase) {
-          return (currentItem.toString().charAt(0).toUpperCase() + currentItem.toString().slice(1));
-        } else {
-          return currentItem;
-        }
-      })();
-      return match;
-    });
-    return textBlobWithReplacements;
-  };
-
-  const boolish = {
-    true: false,
-    false: true,
-    0: 1,
-    1: 0,
-    yes: 'no',
-    no: 'yes',
-    on: 'off',
-    off: 'on',
-  };
-
-  const swappedText = findAndReplace(textBlob, boolish);
-  return swappedText; // re-capitalize
 };
 
 const swapBoolFromSelection = (editor: vscode.TextEditor): void => {
